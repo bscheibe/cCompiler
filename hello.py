0dbcbf7ebdde0c7dd99b8fc7b381df5168350d17
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2015-01-02 15:07:32 -0500

    Upload to heroku

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, url_for, render_template, request, send_from_directory, redirect
+from flask import Flask, url_for, render_template, request, send_from_directory, \
+	redirect, session, escape, flash
 from werkzeug import secure_filename
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return 
+    return "OH NO"
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -49,7 +50,8 @@ with app.test_request_context():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name="Brent"):
-    return render_template('hello.html', name=name)
+	username = request.cookies.get('username')
+	return render_template('hello.html', name=name)
 
 with app.test_request_context('/cool', method='POST'):
     # now you can do something with the request until the
@@ -57,11 +59,46 @@ with app.test_request_context('/cool', method='POST'):
     assert request.path == '/cool'
     assert request.method == 'POST'
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+# Login stuff
+@app.route('/home')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        flash('You were successfully logged in')
+        return redirect(url_for('index'))
+    else:
+    	return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = '\x83\xfa=\x1a*\x98\x93\xa6\x9c\x06W%\xbe=\x15\x08b\xd5_t$X\x7fQ'
+
+# End of Login stuff
+
 """@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['cool.txt']
         f.save('/var/www/uploads/uploaded_file.txt')"""
+
+"""app.logger.debug('A value for debugging')
+        app.logger.warning('A warning occurred (%d apples)', 42)
+        app.logger.error('An error occurred')"""
 
 if __name__ == '__main__':
     app.run(debug=True)
