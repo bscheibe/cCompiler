ed642a99e3b67b3a0c985270f4c2a4ac9b79cc39
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2015-01-02 13:22:15 -0500

    Upload working

--- hello.py
+++ hello.py
@@ -1,9 +1,37 @@
-from flask import Flask, url_for, render_template, request
+import os
+from flask import Flask, url_for, render_template, request, send_from_directory, redirect
+from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/')
+UPLOAD_FOLDER = '/Users/schybo/Documents/Repos/Apps/FlaskApp/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print "cool"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return 
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+"""@app.route('/')
 def index():
-    return 'Index Page'
+    return 'Index Page'"""
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -28,6 +56,12 @@ with app.test_request_context('/cool', method='POST'):
     # end of the with block, such as basic assertions:
     assert request.path == '/cool'
     assert request.method == 'POST'
+
+"""@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['cool.txt']
+        f.save('/var/www/uploads/uploaded_file.txt')"""
 
 if __name__ == '__main__':
     app.run(debug=True)
